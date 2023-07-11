75ec16cd05278b7aed8bc7e91eecd7833ad4cc37
Eric Kim-Butler <eric.butler@notarize.com>
2023-07-11 14:00:27 +0200

    Improve CI

---
+++ .circleci/bin/build-release-binary.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+. "$CIRCLE_WORKING_DIRECTORY/.circleci/bin/get-release-platforms.sh"
+
+for PLATFORM in "${RELEASE_PLATFORMS[@]}"
+do
+  echo "*** Building release binary for $PLATFORM ***"
+  if [[ "$PLATFORM" =~ "-apple-darwin" ]]; then
+    CC="o64-clang" CXX="o64-clang++" LIBZ_SYS_STATIC="1" cargo build --release "--target=$PLATFORM"
+  else
+    cargo build --release "--target=$PLATFORM"
+  fi
+done
